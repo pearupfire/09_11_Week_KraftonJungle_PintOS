2e32564c710fd2b40c7b146a7a66542d38746c26
Suin Kim <contact@suinkim.me>
2025-07-21 14:49:30 +0900

    chore: 구성 편집 USERPROG define 추가

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -59,6 +59,7 @@ process_create_initd (const char *file_name) {
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
 		palloc_free_page (fn_copy);
+
 	return tid;
 }
 
